2e4e8ec325037ed37c739492e7852bdb7cf9952f
Shyam <shyambhuva4@gmail.com>
2023-03-15 18:05:49 +0530

    data ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+import os
+import sys
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+##logger
+# from datetime import datetime
+
+# LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# logs_path=os.path.join(os.getcwd(),'logs',LOG_FILE)
+# os.makedirs(logs_path,exist_ok=True)
+
+# LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+# logging.basicConfig(
+# filename=LOG_FILE_PATH,
+# format='[ %(asctime)s ] %(lineno)d %(name)s - %(levelno)s - %(message)s',
+# level=logging.INFO
+# )
+
+@dataclass
+class DataIngestionCofig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__(self) :
+        self.ingestion_config=DataIngestionCofig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion")
+
+        try:
+         df=pd.read_csv('src/notebook/data/stud.csv')
+         logging.info('Read dataset')
+
+         os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+         df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+         logging.info("Split initiated")
+
+         train_set,test_set=train_test_split(df,test_size=0.20,random_state=42)
+         train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+         test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+         logging.info('Data ingestion is completed')
+
+         return (self.ingestion_config.train_data_path,
+                 self.ingestion_config.test_data_path)
+
+
+        except Exception as e:
+           raise CustomException(e,sys)
+        
+if __name__=='__main__':
+   obj=DataIngestion()
+   obj.initiate_data_ingestion()
